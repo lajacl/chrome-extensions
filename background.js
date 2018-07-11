6e7abad2a625d7e7522ec5f8e8b83965a606702c
LaVette Clifton <lavette.clifton@gmail.com>
2018-07-11 15:42:07 -0500

    timer starts when restricted site is accessed and badge is updated

--- background.js
+++ background.js
@@ -1,22 +1,74 @@
+let url
+let time = 5;
+let counter;
+let redirect_url;
+const badge_colors = {
+    green: "#0C0",
+    yellow: "#FC3",
+    red: "#C00",
+    gray: "#808080"
+}
+const url_patterns = [
+    "*://*.facebook.com/*",
+    "*://*.messenger.com/*",
+    "*://*.instagram.com/*",
+    "*://*.twitter.com/*",
+    "*://*.whatsapp.com/*",
+    "*://*.youtube.com/*",
+    "*://*.vimeo.com/*"
+]
+
+function startTimer() {    
+    counter = setInterval(function() {   
+        if(time > 0) {
+            time--;
+            chrome.browserAction.setBadgeText({text: getTimeString()});
+            if(time == 0) {                
+                chrome.browserAction.setBadgeBackgroundColor({color: badge_colors.red});
+            }
+        } else {     
+            if(counter){
+                clearInterval(counter);   
+            }
+        }   
+    }, 1000)
+}
+
+function getTimeString() {
+    return ("0000" + time).slice(-4);
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.browserAction.setBadgeText({text: "-"});
+    chrome.browserAction.setBadgeBackgroundColor({color: badge_colors.gray});
+});
+
 chrome.webRequest.onBeforeRequest.addListener(
-function(details) {
-    console.log("Website intercepted: " + details.url);
-    // Redirect the lolcal request to a random loldog URL.
-    // var i = Math.round(Math.random() * loldogs.length);
-    return {redirectUrl: "https://www.google.com"};
-    // return {cancel: true}
+function(details) { 
+    url = details.url;
+    console.log("Flagged Website: " + url);
+
+    chrome.browserAction.setBadgeText({text: getTimeString()});
+
+    if(time > 0) {
+        chrome.browserAction.setBadgeBackgroundColor({color: badge_colors.green});
+        if(!counter){
+        startTimer();   
+        }   
+    }    
+    
+    if(time <= 0) {        
+        if (typeof redirect_url !== "undefined") {
+            return {redirectUrl: redirect_url};
+        } else {
+            return {cancel: true}
+        }
+    }
 },
 // filters
 {
-    urls: [
-        "*://*.facebook.com/*",
-        "*://*.messenger.com/*",
-        "*://*.instagram.com/*",
-        "*://*.twitter.com/*",
-        "*://*.whatsapp.com/*",
-        "*://*.youtube.com/*",
-        "*://*.vimeo.com/*"
-    ]
+    urls: url_patterns
 },
 // extraInfoSpec
-["blocking"]);
+["blocking"]
+);
